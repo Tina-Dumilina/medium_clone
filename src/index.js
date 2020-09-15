3ad2f5d7f141e9841dfb190b29ae1a640fd73b9b
Tina Dumilina <tina.dumilina@gmail.com>
2020-09-15 22:12:35 +0300

    Added navigation and authentification page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Routes from 'routes';
+import {BrowserRouter} from 'react-router-dom';
+import TopBar from 'components/topBar';
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <TopBar />
+        <Routes />
+      </BrowserRouter>
+    </div>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +22,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
