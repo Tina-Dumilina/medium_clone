ebfb39aa9e591bea176ca81961660e606dd8c4c7
Tina Dumilina <tina.dumilina@gmail.com>
2020-09-20 21:52:54 +0300

    Added currentUser context

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom';
 import Routes from 'routes';
 import {BrowserRouter} from 'react-router-dom';
 import TopBar from 'components/topBar';
+import {CurrentUserProvider} from 'contexts/currentUser'
+import CurrentUserChecker from 'components/currentUserChecker'
 
 const App = () => {
   return (
-    <div>
-      <BrowserRouter>
-        <TopBar />
-        <Routes />
-      </BrowserRouter>
-    </div>
+    <CurrentUserProvider>
+      <CurrentUserChecker>
+        <BrowserRouter>
+          <TopBar />
+          <Routes />
+        </BrowserRouter>
+      </CurrentUserChecker>
+    </CurrentUserProvider>
   )
 }
 
